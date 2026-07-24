5c74fd80ad7c17aa8ef9f73aba4abc5fb573fc82
Maggie Tseng <maggie_tseng@genixnetworks.com>
2026-07-24 17:05:30 +0800

    feat: implement Navigation component with tab switching functionality

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { FiHome, FiBriefcase, FiFileText, FiCamera, FiMail } from "react-icons/fi";
 import "./Navigation.css";
 
